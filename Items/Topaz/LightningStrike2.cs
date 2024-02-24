ebb93b2fd4d629cb8be09de7ae30d85caf0e3c41
davewadd <43561365+davewadd@users.noreply.github.com>
2024-02-24 16:56:46 -0600

    Multiplayer fixes
    Fixed some multiplayer bugs but it is still kinda wonky lol

--- Items/Topaz/LightningStrike2.cs
+++ Items/Topaz/LightningStrike2.cs
@@ -5,81 +5,77 @@ using System;
 
 using Terraria;
 using SariaMod.Buffs;
-using SariaMod.Dusts;
+
+
 
 
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace SariaMod.Items.Ruby
+namespace SariaMod.Items.Topaz
 {
-	public class Explosion3 : ModProjectile
+	public class LightningStrike2 : ModProjectile
 	{
 		public override void SetStaticDefaults()
 		{
 			base.DisplayName.SetDefault("Blade");
 			ProjectileID.Sets.TrailCacheLength[base.projectile.type] = 6;
 			ProjectileID.Sets.TrailingMode[base.projectile.type] = 0;
-			Main.projFrames[base.projectile.type] = 5;
+			Main.projFrames[base.projectile.type] = 4;
 		}
 
 		public override void SetDefaults()
 		{
-			base.projectile.width = 300;
-			base.projectile.height = 300;
-			base.projectile.aiStyle = 21;
+			base.projectile.width = 40;
+			base.projectile.height = 400;
+			
 			base.projectile.alpha = 100;
 			base.projectile.friendly = true;
 			base.projectile.tileCollide = false;
 			
 			base.projectile.penetrate = -1;
-			base.projectile.timeLeft = 200;
+			base.projectile.timeLeft = 150;
 			base.projectile.ignoreWater = true;
-			aiType = 274;
+			
 			base.projectile.usesLocalNPCImmunity = true;
-			base.projectile.localNPCHitCooldown = 20;
+			base.projectile.localNPCHitCooldown = 10;
 		}
-		private const int sphereRadius = 100;
+
 		public override void AI()
 		{
 			Player player = Main.player[base.projectile.owner];
 			FairyPlayer modPlayer = player.Fairy();
-			Lighting.AddLight(base.projectile.Center, 20f, 5f, 0f);
-			FairyGlobalProjectile.HomeInOnNPC(base.projectile, ignoreTiles: true, 600f, 25f, 20f);
-			if (Main.rand.NextBool())//controls the speed of when the sparkles spawn
-			{
-				for (int d = 0; d < 8; d++)
-				{
-					float radius = (float)Math.Sqrt(Main.rand.Next(sphereRadius * sphereRadius));
-					double angle = Main.rand.NextDouble() * 5.0 * Math.PI;
-					Dust.NewDust(new Vector2(projectile.Center.X + radius * (float)Math.Cos(angle), (projectile.Center.Y - 10) + radius * (float)Math.Sin(angle)), 0, 0, ModContent.DustType<FlameDust>(), 0f, 0f, 0, default(Color), 1.5f);
-
-				}
-			}
-				if (Main.rand.NextBool())//controls the speed of when the sparkles spawn
-				{
-					for (int d = 0; d < 3; d++)
-					{
-						Projectile.NewProjectile(base.projectile.Center + new Vector2(0f, 0f), Vector2.One.RotatedByRandom(6) * 3f, ModContent.ProjectileType<Smokeball>(), base.projectile.damage, base.projectile.knockBack, player.whoAmI, base.projectile.whoAmI);
-
-					}
-				}
 			
+			FairyGlobalProjectile.HomeInOnNPC(base.projectile, ignoreTiles: true, 600f, 25f, 20f);
+			Lighting.AddLight(projectile.Center, Color.OrangeRed.ToVector3() * 0.78f);
 			if (player.HasBuff(ModContent.BuffType<Overcharged>()))
 			{
-				projectile.width = 450;
+				projectile.width = 100;
 				projectile.height = 450;
-				projectile.scale = 1.5f;
-				projectile.localNPCHitCooldown = 25;
+				projectile.localNPCHitCooldown = 6;
 			}
-			Lighting.AddLight(projectile.Center, Color.OrangeRed.ToVector3() * 0.78f);
 			{
+
+
+
+				if (projectile.timeLeft >= 140 && projectile.timeLeft <= 150)
+				{
+					Projectile.NewProjectile(base.projectile.Center + new Vector2(0f, -120f), Vector2.One.RotatedByRandom(6.2831854820251465) * 2f, ModContent.ProjectileType<Fiz>(), base.projectile.damage, base.projectile.knockBack, player.whoAmI, base.projectile.whoAmI);
+					
+				}
 				
-				projectile.knockBack = 50;
-				base.projectile.velocity.X = (1 * player.direction);
-				base.projectile.velocity.Y = 0;
+				if (projectile.timeLeft >= 190)
+				{
+					base.projectile.velocity.X = (1 * player.direction);
+					base.projectile.velocity.Y = 0;
+				}
+				else if (projectile.timeLeft < 190)
+					{
+						base.projectile.velocity.X = 0;
+						base.projectile.velocity.Y = 0;
+					}
 				base.projectile.frameCounter++;
-				if (base.projectile.frameCounter >= 5)
+				if (base.projectile.frameCounter >= 4)
 				{
 					base.projectile.frame++;
 					base.projectile.frameCounter = 0;
@@ -87,32 +83,20 @@ namespace SariaMod.Items.Ruby
 				}
 				if (base.projectile.frame >= Main.projFrames[base.projectile.type])
 				{
-					base.projectile.frame = 0;
-					base.projectile.Kill();
-				}
-				if (base.projectile.timeLeft == 195)
-				{
-					
-						if (player.ownedProjectileCounts[ModContent.ProjectileType<Flame>()] < 60f)
-						{
-							for (int j = 0; j < 12; j++) //set to 2
-							{
-								Projectile.NewProjectile(base.projectile.Center + Utils.RandomVector2(Main.rand, -204f, 24f), Vector2.One.RotatedByRandom(6.2831854820251465) * 20f, ModContent.ProjectileType<Flame>(), base.projectile.damage, base.projectile.knockBack, player.whoAmI, base.projectile.whoAmI);
-							}
-						}
-					
+					base.projectile.frame = 3;
 					
 				}
+			
 			}
-			if (projectile.timeLeft >= 200)
+			
+			
+			
+			if (projectile.timeLeft >= 150)
             {
 				Main.PlaySound(SoundID.Item116, base.projectile.Center);
-				Main.PlaySound(base.mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Bomb"), base.projectile.Center);
+				Main.PlaySound(base.mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Lightning"), base.projectile.Center);
 			}
-			if (projectile.timeLeft == 2)
-			{
-				Main.PlaySound(SoundID.DD2_SkyDragonsFuryShot, base.projectile.Center);
-			}
+			
 		}
 
 		public override Color? GetAlpha(Color lightColor)
@@ -131,7 +115,7 @@ namespace SariaMod.Items.Ruby
 			FairyGlobalProjectile.DrawCenteredAndAfterimage(base.projectile, lightColor, ProjectileID.Sets.TrailingMode[base.projectile.type]);
 			return false;
 		}
-
+		
 		public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
 		{
 			Player player = Main.player[base.projectile.owner];
@@ -149,11 +133,8 @@ namespace SariaMod.Items.Ruby
 			target.buffImmune[BuffID.Electrified] = false;
 			target.buffImmune[ModContent.BuffType<Burning2>()] = false;
 			target.AddBuff(ModContent.BuffType<Burning2>(), 200);
+			target.AddBuff(BuffID.Electrified, 300);
 			modPlayer.SariaXp++;
-			if (target.type == NPCID.Mothron|| target.type == NPCID.MourningWood || target.type == NPCID.Everscream)
-            {
-				damage *= 4;
-            }
 			if (player.HasBuff(ModContent.BuffType<StatRaise>()))
 			{
 				damage += (damage) / 4;
@@ -163,6 +144,7 @@ namespace SariaMod.Items.Ruby
 				damage /= 2;
 
 			}
+			knockback /= 1000;
 		}
 
 
